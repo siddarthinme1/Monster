d1409a8dd617ab7014db5c0bc136a275d5369a10
Siddarood Birappa Karachuri <siddarood.karachuri@ltts.com>
2023-09-29 15:29:49 +0530

    notification bar fix

--- src/Components/SpeedDial/SpeedDialX.jsx
+++ src/Components/SpeedDial/SpeedDialX.jsx
@@ -1,23 +1,13 @@
 import React, { useState } from "react";
-
 import SpeedDial from "@mui/material/SpeedDial";
-
 import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-
 import SpeedDialAction from "@mui/material/SpeedDialAction";
-
 import SaveIcon from "@mui/icons-material/Save";
-
 import PrintIcon from "@mui/icons-material/Print";
-
 import ShareIcon from "@mui/icons-material/Share";
-
 import AddIcon from "@mui/icons-material/Add";
-
 import EditIcon from "@mui/icons-material/Edit";
-
 import AppBarContext from "../../Context/AppBarContext";
-
 import {
   Slide,
   Dialog,
@@ -29,23 +19,16 @@ import {
 
 const actions = [
   { icon: <AddIcon />, name: "Add" },
-
   { icon: <SaveIcon />, name: "Save" },
-
   { icon: <PrintIcon />, name: "Print" },
-
   { icon: <ShareIcon />, name: "Share" },
 ];
 
 export default function SpeedDialX() {
   const [open, setOpen] = React.useState(false);
-
-  const [currentPage, setCurrentPage] = useState("default"); // Add a state for current page
-
-  const [dialogOpen, setDialogOpen] = useState(false); // Add state for dialog
-
+  const [currentPage, setCurrentPage] = useState("default");
+  const [dialogOpen, setDialogOpen] = useState(false);
   const { trigger } = React.useContext(AppBarContext);
-
   const handleOpen = () => {
     setOpen(true);
   };
@@ -56,34 +39,23 @@ export default function SpeedDialX() {
 
   const handlePageChange = (pageName) => {
     setCurrentPage(pageName);
-
-    // Open the dialog when a button is clicked
-
     setDialogOpen(true);
   };
 
   const handleDialogClose = () => {
-    // Close the dialog when needed
-
     setDialogOpen(false);
   };
-
-  // Render different pages based on the currentPage state
 
   const renderPage = () => {
     switch (currentPage) {
       case "Add":
         return <div>Add Page Content</div>;
-
       case "Save":
         return <div>Save Page Content</div>;
-
       case "Print":
         return <div>Print Page Content</div>;
-
       case "Share":
         return <div>Share Page Content</div>;
-
       default:
         return <div>Default Page Content</div>;
     }
@@ -92,8 +64,7 @@ export default function SpeedDialX() {
   return (
     <Slide appear={false} direction="left" in={!trigger}>
       <div>
-        {renderPage()}
-
+        {renderPage}
         <SpeedDial
           ariaLabel="SpeedDial example"
           sx={{ position: "fixed", bottom: 75, right: 16 }}
@@ -107,22 +78,15 @@ export default function SpeedDialX() {
               key={action.name}
               icon={action.icon}
               tooltipTitle={action.name}
-              onClick={() => handlePageChange(action.name)} // Change the current page on click
+              onClick={() => handlePageChange(action.name)}
             />
           ))}
         </SpeedDial>
-
-        {/* Dialog */}
-
         <Dialog open={dialogOpen} onClose={handleDialogClose}>
           <DialogTitle>{currentPage} Dialog</DialogTitle>
-
           <DialogContent>
-            {/* You can put content specific to the current page inside the dialog */}
-
             <div>Content for {currentPage}</div>
           </DialogContent>
-
           <DialogActions>
             <Button onClick={handleDialogClose}>Close</Button>
           </DialogActions>
