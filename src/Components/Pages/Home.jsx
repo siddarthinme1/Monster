9a915d0d4a64449c5816772055304b97ff5efde0
Siddarood Birappa Karachuri <siddarood.karachuri@ltts.com>
2023-10-18 10:38:52 +0530

    signin signup page fix

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -6,7 +6,15 @@ function Home() {
   return (
     <div>
       <Grid container component="main" sx={{ height: "100vh" }}>
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          component={Paper}
+          elevation={6}
+          square
+        >
           <Box
             sx={{
               my: 8,
